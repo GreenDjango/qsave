921f1cb2e53b16d38f91c0a4ed31aea08c340c6e
Theo <theo.cousinet@epitech.eu>
2021-05-09 22:46:12 +0200

    ADD: loader & search logic

---
+++ src/components/Loader.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex justify-center items-center">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+      />
+    </svg>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component'
+
+export default class Loader extends Vue {}
+</script>
+
+<style scoped>
+svg {
+  animation: 2s linear 0s infinite loader;
+}
+
+@keyframes loader {
+  0% {
+    stroke: hsl(var(--af));
+    transform: rotateZ(0);
+  }
+  50% {
+    stroke: hsl(var(--a));
+  }
+  100% {
+    stroke: hsl(var(--af));
+    transform: rotateZ(-360deg);
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
           <Icon :glyph="stat.glyph" class="inline-block w-12 stroke-current mr-6" />
           <div>
             <h4 class="text-xl card-title mb-1">{{ stat.title }}</h4>
-            <span class="text-lg font-bold">{{ stat.text }}</span>
+            <span class="text-lg font-bold">{{ stat.value }}{{ stat.text }}</span>
           </div>
         </div>
       </div>
@@ -15,18 +15,35 @@
     <div class="card col-span-1 row-span-3 shadow-lg bg-base-100">
       <div class="card-body px-6">
         <div class="w-full flex items-center justify-evenly flex-col md:flex-row">
-          <div class="relative w-full sm:w-2/3 md:w-1/3">
-            <select class="select select-bordered w-full">
-              <option disabled="" selected="">Choose a tag</option>
-              <option>any</option>
-              <option>telekinesis</option>
-              <option>time travel</option>
-              <option>invisibility</option>
+          <ul v-show="Object.keys(selectTags).length" class="flex flex-wrap justify-center tags-list w-full md:w-1/4">
+            <li
+              v-for="(tag, key) in selectTags"
+              :key="key"
+              @click="onRemoveTag(key)"
+              class="badge badge-accent badge-outline cursor-pointer mb-1"
+            >
+              {{ tag }}
+              <Icon glyph="x" class="inline-block w-4 h-4 ml-2 stroke-current" />
+            </li>
+          </ul>
+
+          <div class="relative w-full sm:w-2/3 md:w-1/3 mt-3 md:mt-0">
+            <select v-model="tagPicker" class="select select-bordered w-full">
+              <option disabled="" selected="" value="-1">Choose a tag</option>
+              <option v-for="(tag, key) in tags" :key="key" :value="key">
+                {{ tag }}
+              </option>
             </select>
           </div>
-          <div class="relative w-full sm:w-2/3 md:w-1/3 mb-3 md:mb-0">
-            <input type="text" placeholder="Search" class="w-full pr-16 text-base input input-primary input-bordered" />
-            <button class="absolute right-0 rounded-l-none btn btn-primary">
+
+          <div class="relative w-full sm:w-2/3 md:w-1/3 mt-3 md:mt-0 mb-3 md:mb-0">
+            <input
+              ref="searchBox"
+              type="text"
+              placeholder="Search"
+              class="w-full pr-16 text-base input input-primary input-bordered"
+            />
+            <button @click="onSearch" :disabled="loading" class="absolute right-0 rounded-l-none btn btn-primary">
               <Icon glyph="search" class="inline-block w-6 stroke-current" />
             </button>
           </div>
@@ -34,7 +51,11 @@
 
         <h2 class="my-4 text-2xl font-bold card-title">Qnotes</h2>
 
-        <Table :items="items" @row-click="onRowClick" />
+        <Loader v-if="loading" class="w-full my-6" />
+
+        <Table v-else-if="items && items.length" :items="items" @row-click="onRowClick" />
+
+        <div v-else class="w-full text-center my-6">No Qnotes found</div>
 
         <!--
         <div class="btn-group mt-4 justify-center">
@@ -59,6 +80,7 @@ import copy from 'copy-text-to-clipboard'
 import { Options, Vue } from 'vue-class-component'
 import Icon from '@/components/Icon.vue'
 import Table from '@/components/Table.vue'
+import Loader from '@/components/Loader.vue'
 import ColorPallet from '@/components/ColorPallet.vue'
 
 type Qnote = { id: number; date: Date; tags: string[]; url?: string; text?: string; code?: string }
@@ -67,15 +89,24 @@ type Qnote = { id: number; date: Date; tags: string[]; url?: string; text?: stri
   components: {
     Icon,
     Table,
+    Loader,
     ColorPallet,
   },
 })
 export default class Home extends Vue {
+  loading = true
   stats = [
-    { glyph: 'bookmark-alt', title: 'Total Qnotes', text: '103' },
-    { glyph: 'tag', title: 'Total tags', text: '56' },
-    { glyph: 'database', title: 'Database size', text: '10ko' },
+    { glyph: 'bookmark-alt', title: 'Total Qnotes', value: '103', text: '' },
+    { glyph: 'tag', title: 'Total tags', value: '56', text: '' },
+    { glyph: 'database', title: 'Database size', value: '10', text: 'ko' },
   ]
+  tags = {
+    1: 'telekinesis',
+    2: 'time travel',
+    3: 'invisibility',
+  } as any
+  tagPicker = '-1'
+  selectTags = {} as any
   items: Qnote[] = [
     { id: 1, date: new Date(2021, 3, 10), tags: ['url', 'UI-Design', 'Creativity'], url: 'https://www.google.com/' },
     { id: 2, date: new Date(), tags: ['url', 'UI-Design', 'Creativity'], url: 'https://www.google.com/' },
@@ -84,12 +115,48 @@ export default class Home extends Vue {
     { id: 5, date: new Date(2021, 3, 11), tags: ['url', 'UI-Design', 'Creativity'], url: 'https://www.google.com/' },
   ]
 
+  beforeMount() {
+    this.$watch('tagPicker', this.onTagPicker)
+    this.fetchQnotes()
+  }
+
+  onTagPicker(newVal: string, oldVal: string) {
+    if (newVal === '-1') return
+    this.tagPicker = '-1'
+    this.selectTags[newVal] = this.tags[newVal]
+    delete this.tags[newVal]
+  }
+
+  onRemoveTag(id: string) {
+    this.tags[id] = this.selectTags[id]
+    delete this.selectTags[id]
+  }
+
+  onSearch() {
+    console.log((this.$refs.searchBox as HTMLInputElement).value, Object.keys(this.selectTags))
+    this.fetchQnotes()
+  }
+
   onRowClick(row: Qnote) {
     const dataToCopy = row.url || row.text || row.code
     if (dataToCopy) {
       copy(String(dataToCopy))
       this.notify('Copied !', { type: 'info', queue: false, duration: 1500 })
     }
+  }
+
+  async fetchQnotes() {
+    const query = new Promise((resolve) => {
+      setTimeout(() => resolve(1), 1500)
+    })
+    this.loading = true
+    try {
+      await query
+    } catch (err) {
+      this.items = []
+      this.notify(err, { title: 'Error', type: 'error', duration: 2500 })
+    }
+    this.loading = false
   }
 
   notify(
@@ -102,4 +169,7 @@ export default class Home extends Vue {
 </script>
 
 <style scoped>
+.tags-list li:not(:last-child) {
+  margin-right: 0.25rem;
+}
 </style>
